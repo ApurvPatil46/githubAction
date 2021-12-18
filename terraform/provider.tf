15609ba5ff8c42d8e536e40505b403e3c002326e
Apurv Nilkanth Patil <52930645+ApurvPatil46@users.noreply.github.com>
2021-12-18 21:23:38 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,28 +13,6 @@ terraform {
 
 resource "aws_s3_bucket" "s3Bucket" {
   bucket = "host-website-with-github-ci"
-  acl    = "public-read"
-
-  policy = <<EOF
-{
-  "Id": "BucketPolicy",
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "AllAccess",
-      "Action": "s3:*",
-      "Effect": "Allow",
-      "Resource": [
-         "arn:aws:s3:::host-website-with-github-ci",
-         "arn:aws:s3:::host-website-with-github-ci/*"
-      ],
-      "Principal": "*"
-    }
-  ]
-}
-EOF
-
-  website {
-    index_document = "pricing.html"
+  acl    = "private"
   }
 }
