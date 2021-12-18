e6f9e468849f94b18d9e9018de8276ff6d9be861
ApurvPatil46 <apurvpatil99946@gmail.com>
2021-12-18 19:37:12 +0530

    Build github/workflow to setup aws s3 bucket

---
+++ terraform/provider.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-remote-state-aws-ci"
+    key    = "website"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_s3_bucket" "s3Bucket" {
+  bucket = "Host-Website-With-Github-Ci"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+  "Id": "MakePublic",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::terraform-remote-state-aws-ci/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+
+  website {
+    index_document = "pricing.html"
+  }
+}
